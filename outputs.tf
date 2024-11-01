c3732951ca81fc86de6d6c3d638c27024516fd32
Henry Cheung <henry.cheung@softwareone.com>
2024-11-01 11:02:37 +0800

    Fix typo

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "configuration" {
   value       = local.configure_connectivity_resources
 }
 
-output "" {
+output "subscription_id" {
   description = "Subscription ID for the \"connectivity\" resources."
   value       = var.subscription_id_connectivity
 }
